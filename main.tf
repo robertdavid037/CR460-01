6b0cb784dc7c0b413a023b0140fa36a1c7b51558
robertdavid037 <robertdavid037@gmail.com>
2024-10-03 20:21:14 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 provider "azurerm" {
   features {}
-  subscription_id = var.36e07cb1-84ef-4d1a-94a1-0caa6c1bb763
-  tenant_id       = var.a9235ce3-98e1-4956-84ea-532e6dd58ca6
+
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
 }
 
 # Groupe de ressources
