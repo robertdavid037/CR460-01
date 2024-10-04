1c49ef84b1870a91d965df524248ef5b0fa28a7d
robertdavid037 <robertdavid037@gmail.com>
2024-10-03 20:21:59 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "subscription_id" {
+  description = "36e07cb1-84ef-4d1a-94a1-0caa6c1bb763"
+  type        = string
+}
+
+variable "a9235ce3-98e1-4956-84ea-532e6dd58ca6" {
+  description = "The Azure tenant ID"
+  type        = string
+}
