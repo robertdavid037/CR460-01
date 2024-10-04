3bf13c29fc1b531362d9d81270ea70be4252da24
robertdavid037 <robertdavid037@gmail.com>
2024-10-03 22:08:34 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,12 @@
 provider "azurerm" {
   features {}
 
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  tenant_id       = var.azure_tenant_id
+  subscription_id  = var.azure_subscription_id
 }
+
 
 # Groupe de ressources
 resource "azurerm_resource_group" "cr460_group" {
